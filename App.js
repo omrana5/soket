bd068d47eef9e9f0e250d7700bad45ad4d6d2082
Pakglobal <pak.globaliasoft@gmail.com>
2022-10-11 11:24:23 +0530

    remove error

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Home';
 import Imagecollection from './src/Imagecollection';
-import Mergeimage from './src/Mergeimage';
+// import Mergeimage from './src/Mergeimage';
 import Customeimageframe from './src/Customeimageframe';
 
 const Stack = createNativeStackNavigator();
